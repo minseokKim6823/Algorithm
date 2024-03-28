15d19589455eca043d067df01d1833f1b9949f36
김민석 <93479041+minseokKim6823@users.noreply.github.com>
2024-03-28 13:13:56 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,24 +1,8 @@
-list1=[[65,66,67],[68,69,70],
-       [71,72,73],[74,75,76],
-       [77,78,79],[80,81,82,83],
-       [84,85,86],[86,87,88,89]]
-result=0
-word=input()
-for i in range(len(word)):
-    if ord(word[i]) in list1[0]:
-        result+=3
-    elif ord(word[i]) in list1[1]:
-        result+=4
-    elif ord(word[i]) in list1[2]:
-        result+=5
-    elif ord(word[i]) in list1[3]:
-        result+=6
-    elif ord(word[i]) in list1[4]:
-        result+=7
-    elif ord(word[i]) in list1[5]:
-        result+=8
-    elif ord(word[i]) in list1[6]:
-        result+=9
-    else:
-        result+=10
-print(result)
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+a = input()
+ret = 0
+for j in range(len(a)):
+    for i in dial:
+        if a[j] in i:
+            ret += dial.index(i)+3
+print(ret)
