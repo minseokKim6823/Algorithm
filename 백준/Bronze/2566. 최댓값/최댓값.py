960f47fb34bc71878506dc3eda0465797b9b5354
김민석 <93479041+minseokKim6823@users.noreply.github.com>
2024-03-29 18:00:32 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,18 +1,15 @@
-field = []
+list1=[]
+result=0
 
 for _ in range(9):
-    field.append(list(map(int, input().split())))
-# 값 하나씩 탐색하며 최댓값을 찾는다.
-max_value = 0
-row = 0
-col = 0
+    list1.append(list(map(int, input().split())))
 
 for i in range(9):
     for j in range(9):
-        if field[i][j] >= max_value:
-            max_value = field[i][j]
-            row = i+1
-            col = j+1
+        if list1[i][j] >= result:
+            result=list1[i][j]
+            r=i+1
+            c=j+1
 
-print(max_value)
-print(row, col)
+print(result)
+print(r, c)
