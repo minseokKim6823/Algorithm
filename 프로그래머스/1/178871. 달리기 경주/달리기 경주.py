14b4b8fb813591307977cef9da0c4df5be22f433
김민석 <93479041+minseokKim6823@users.noreply.github.com>
2025-02-14 16:13:20 +0900

    [level 1] Title: 달리기 경주, Time: 513.05 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -2,7 +2,7 @@ def solution(players, callings):
     answer=[]
     player_dict = {player: idx for idx, player in enumerate(players)}
     for call in callings:
-        idx = player_dict[call]
+        idx = player_dict[call]#4
         players[idx],players[idx-1] = players[idx-1],players[idx]
         player_dict[players[idx]] = idx
         player_dict[players[idx-1]] = idx-1
