cd4bb429f7d6418ae54d878553ba0cf3ac48528a
김민석 <93479041+minseokKim6823@users.noreply.github.com>
2026-01-15 21:20:09 +0900

    [Silver II] Title: 수열의 합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1024. 수열의 합/수열의 합.py
+++ 백준/Silver/1024. 수열의 합/수열의 합.py
@@ -1,18 +1,12 @@
 N, L = map(int, input().split())
-
-found = False
 for i in range(L, 101):
-    base_sum = i * (i - 1) // 2
-    
-    if base_sum > N:
+    s = i * (i - 1) // 2  # 이것도 공식으로 바꾸면 더 깔끔
+    if s > N:
+        print(-1)
         break
-    
-    if (N - base_sum) % i == 0:
-        start = (N - base_sum) // i
-        if start >= 0:  # 시작값이 음수면 안됨
-            print(' '.join(map(str, range(start, start + i))))
-            found = True
-            break
-
-if not found:
+    if (N - s) % i == 0 and (N - s) // i >= 0:
+        start = (N - s) // i
+        print(' '.join(map(str, range(start, start + i))))
+        break
+else:  # for문이 break 없이 끝났을 때
     print(-1)
