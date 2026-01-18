3e6e557ae84851e9820ef11f55d06e377c108ef0
김민석 <93479041+minseokKim6823@users.noreply.github.com>
2026-01-19 00:09:45 +0900

    [Bronze II] Title: 운동, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1173. 운동/운동.py
+++ 백준/Bronze/1173. 운동/운동.py
@@ -1,13 +1,13 @@
 N, m, M, T, R = map(int,input().split())
 # 초기맥박 = m, 증가 맥박 =T, 감소 맥박 =R, 최대 맥박=M
 workout = 0
-time=1
+time=0
 pulse=m
 if m+T>M:
     print(-1)
     exit(0)
+
 while workout != N:
-    time += 1
     if pulse+T <= M:
         pulse += T
         workout += 1
@@ -15,4 +15,5 @@ while workout != N:
         pulse -= R
         if pulse < m:
             pulse = m
-print(time-1)
+    time += 1
+print(time)
