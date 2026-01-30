ed128b25f2262eef6891aaebcc9a169c7ef05e55
김민석 <93479041+minseokKim6823@users.noreply.github.com>
2026-01-30 23:49:34 +0900

    [Silver III] Title: 터렛, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -1,24 +1,21 @@
 import math
+T=int(input())
+for i in range(T):
+    x1,y1,r1,x2,y2,r2=map(int,input().split())
+    d1 = ((x1-x2)**2)+((y1-y2)**2)
+    d2 = (r1+r2) ** 2
+    d3 = (r2-r1) ** 2
 
-T = int(input())
-for _ in range(T):
-    x1, y1, r1, x2, y2, r2 = map(int, input().split())
-
-    dx = x1 - x2
-    dy = y1 - y2
-    d = dx*dx + dy*dy          
-    sum_r = (r1 + r2) ** 2
-    diff_r = (r1 - r2) ** 2
-
-    if dx == 0 and dy == 0 and r1 == r2:
-        print(-1)             
-    elif d > sum_r:
-        print(0)               
-    elif d < diff_r:
-        print(0)               
-    elif d == sum_r:
-        print(1)               
-    elif d == diff_r:
-        print(1)               
+    if x1==x2 and y1 ==y2 and r1==r2:
+        print(-1)
+    elif d1 == d2:
+        print(1)
+    elif d1==d3:
+        print(1)
+    elif d1 > d2:
+        print(0)
+    elif d1 < d3:
+        print(0)
     else:
-        print(2)   
+        print(2)
+    
